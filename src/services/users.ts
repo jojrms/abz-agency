26414e5c258a1c29527d4b472ea78a38963a3702
Jordana <jordanarmos@gmail.com>
2024-08-23 15:46:14 -0300

    add new services

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,13 +1,20 @@
 import axiosInstance from "../config/apiConfig";
-import { GetUsersResult, GetUserByIdResult } from "../types/users";
+import { GetUsersResult, GetUserByIdResult, GetUserPositionsResult, GetUserToken } from "../types/users";
 
 export type UsersStoreService = {
     getUsers: (page: number) => GetUsersResult;
     getUserById: (id: string) => GetUserByIdResult;
+    getToken: () => GetUserToken;
 };
 
 export const usersService = (): UsersStoreService => {
     return {
+        getToken: async() => {
+            const response = await axiosInstance.get('/token');
+            localStorage.setItem('authToken', response.data.token);
+
+            return response;
+        },
         getUsers: async(page: number) => {
             const response = await axiosInstance.get('/users', {params: {page: page, count: 6}});
 
@@ -18,5 +25,22 @@ export const usersService = (): UsersStoreService => {
 
             return response.data;
         }
+    }
+}
+
+// ~~~ POSITIONS ~~~ 
+
+export type UserPositionsStoreService = {
+    getUserPositions: () => GetUserPositionsResult;
+};
+
+export const positionsService = (): UserPositionsStoreService => {
+    return {
+        getUserPositions: async() => {
+            const response = await axiosInstance.get('/positions');
+
+            return response;
+        },
+        
     }
 }
